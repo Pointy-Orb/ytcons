fe8b1e4a8fc91c412f334cb4821f635b19d4cd8e
Pointy Orb <pointyorb7@gmail.com>
2025-07-04 14:00:10 -0400

    Archiving exists now and the feed can now be saved
    The variables all piggyback on variables written by Akregator. Still
    more features to add, but remembering the feed makes it usable!

--- MenuBlocks/FeedChannelMenu.cs
+++ MenuBlocks/FeedChannelMenu.cs
@@ -1,8 +1,18 @@
 using System.Xml;
 using Newtonsoft.Json;
 using System.Collections.Concurrent;
+using YTCons.Scenes;
 
 namespace YTCons.MenuBlocks;
+
+public enum ArchiveMode
+{
+    Default,
+    LimitAge,
+    LimitNumber,
+    ArchiveAll,
+    DontArchive
+}
 
 public class FeedChannelMenu : MenuBlock
 {
@@ -12,22 +22,124 @@ public class FeedChannelMenu : MenuBlock
 
     public string id = "";
 
-    public int maxAritcleAge;
+    public string url = "";
 
-    public int maxArticleNumber;
+    private int _maxArticleAge;
 
-    public bool destroyTheElderly = false;
+    private int _maxArticleNumber;
+
+    public int MaxArticleAge
+    {
+        get
+        {
+            if (archiveMode == ArchiveMode.Default && Globals.activeScene is FeedScene feed)
+            {
+                return feed.maxArticleAge;
+            }
+            return _maxArticleAge;
+        }
+    }
+
+    public int MaxArticleAgeRaw { get => _maxArticleAge; }
+    public int MaxArticleNumberRaw { get => _maxArticleNumber; }
+
+    public int MaxArticleNumber
+    {
+        get
+        {
+            if (archiveMode == ArchiveMode.Default && Globals.activeScene is FeedScene feed)
+            {
+                return feed.maxArticleNumber;
+            }
+            return _maxArticleNumber;
+        }
+    }
 
     public bool notify = false;
     public bool lowPriority = false;
+
+    private bool LimitNumber
+    {
+        get
+        {
+            return archiveMode == ArchiveMode.LimitNumber || (archiveMode == ArchiveMode.Default && Globals.activeScene is FeedScene feed && feed.defaultArchive == ArchiveMode.LimitNumber);
+        }
+    }
+
+    private bool LimitAge
+    {
+        get
+        {
+            return archiveMode == ArchiveMode.LimitAge || (archiveMode == ArchiveMode.Default && Globals.activeScene is FeedScene feed && feed.defaultArchive == ArchiveMode.LimitAge);
+        }
+    }
+
+    private bool DontArchive
+    {
+        get
+        {
+            return archiveMode == ArchiveMode.DontArchive || (archiveMode == ArchiveMode.Default && Globals.activeScene is FeedScene feed && feed.defaultArchive == ArchiveMode.DontArchive);
+        }
+    }
+
+    public ArchiveMode archiveMode = ArchiveMode.Default;
+
+    private readonly char[] caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+    public string PrettifyArchiveMode(ArchiveMode archiveMode, bool addNumbers = true)
+    {
+        switch (archiveMode)
+        {
+            case ArchiveMode.LimitNumber:
+                var prettyNum = $"Limit Article Number";
+                if (addNumbers)
+                {
+                    prettyNum += $" ({_maxArticleNumber})";
+                }
+                return prettyNum;
+            case ArchiveMode.LimitAge:
+                var prettyAge = $"Limit Article Age";
+                if (addNumbers)
+                {
+                    prettyAge += $" ({_maxArticleAge} days)";
+                }
+                return prettyAge;
+            case ArchiveMode.DontArchive:
+                return "Don't Archive";
+            case ArchiveMode.Default:
+            case ArchiveMode.ArchiveAll:
+            default:
+                var pretty = "";
+                var archiveString = archiveMode.ToString().ToCharArray();
+                for (int i = 0; i < archiveString.Length; i++)
+                {
+                    if (caps.Contains(archiveString[i]) && i > 0)
+                    {
+                        pretty += " ";
+                    }
+                    pretty += archiveString[i];
+                }
+                return pretty;
+        }
+    }
+
+    public string PrettyArchiveMode
+    {
+        get
+        {
+            return PrettifyArchiveMode(archiveMode);
+        }
+    }
 
     public class PreMenuPacket
     {
         public required string url;
         public required string title;
-        public int maxAritcleAge = 60;
+        public int maxArticleAge = 60;
         public int maxArticleNumber = 1000;
         public bool notify;
+        public bool lowPriority = false;
+        public ArchiveMode archiveMode = ArchiveMode.Default;
     }
 
     public static async Task<FeedChannelMenu> CreateAsync(PreMenuPacket packet)
@@ -35,15 +147,14 @@ public class FeedChannelMenu : MenuBlock
         var instance = new FeedChannelMenu()
         {
             title = packet.title,
-            maxAritcleAge = packet.maxAritcleAge,
-            maxArticleNumber = packet.maxArticleNumber,
-            notify = packet.notify
+            url = packet.url,
+            _maxArticleAge = packet.maxArticleAge,
+            _maxArticleNumber = packet.maxArticleNumber,
+            notify = packet.notify,
+            archiveMode = packet.archiveMode
         };
         instance.id = packet.url.Replace("https://www.youtube.com/feeds/videos.xml?channel_id=", "");
-        if (instance.destroyTheElderly)
-        {
-            instance.CheckAnnihilation();
-        }
+        instance.CheckAnnihilation();
         List<FeedVideoOption> videos = await FeedVideoOption.CreateGroupAsync(instance, packet.url);
         foreach (FeedVideoOption video in videos)
         {
@@ -54,7 +165,14 @@ public class FeedChannelMenu : MenuBlock
         {
             if (instance.options.Find(i => i is FeedVideoOption option && option.feedData.id == Path.GetFileNameWithoutExtension(filePath)) == null)
             {
-                instance.options.Add(new FeedVideoOption(instance, filePath));
+                if (instance.DontArchive)
+                {
+                    File.Delete(filePath);
+                }
+                else
+                {
+                    instance.options.Add(new FeedVideoOption(instance, filePath));
+                }
             }
         }
         instance.options.Sort((left, right) =>
@@ -107,6 +225,38 @@ public class FeedChannelMenu : MenuBlock
         Globals.activeScene.PopMenu();
     }
 
+    public void SetArchiveMode(ArchiveMode archiveMode, MenuBlock inputPlacement, MenuOption display)
+    {
+        this.archiveMode = archiveMode;
+        if (archiveMode == ArchiveMode.LimitAge || archiveMode == ArchiveMode.LimitNumber)
+        {
+            int num = 0;
+            while (num <= 0)
+            {
+                var selDrawPos = inputPlacement.selectedDrawPos;
+                var isANumber = int.TryParse(Globals.ReadLine(selDrawPos.x, selDrawPos.y, $" > Enter the maximum article {(archiveMode == ArchiveMode.LimitAge ? "age in days" : "number")}"), out var test);
+                if (isANumber)
+                {
+                    num = test;
+                }
+            }
+            switch (archiveMode)
+            {
+                case ArchiveMode.LimitAge:
+                    _maxArticleAge = num;
+                    break;
+                case ArchiveMode.LimitNumber:
+                    _maxArticleNumber = num;
+                    break;
+            }
+        }
+        if (Globals.activeScene is FeedScene feed)
+        {
+            feed.SaveAsXml();
+        }
+        Globals.activeScene.PopMenu();
+    }
+
     public void ChangePriority(MenuOption display)
     {
         lowPriority = !lowPriority;
@@ -119,6 +269,10 @@ public class FeedChannelMenu : MenuBlock
             }
         }
         display.option = $"Switch Priority (current: {(lowPriority ? "Low" : "Normal")})";
+        if (Globals.activeScene is FeedScene feed)
+        {
+            feed.SaveAsXml();
+        }
     }
 
     private void CheckAnnihilation()
@@ -133,12 +287,12 @@ public class FeedChannelMenu : MenuBlock
         int deleted = 0;
         for (int i = 0; i < files.Count(); i++)
         {
-            if (DateTime.Now - File.GetCreationTime(files[i]) > TimeSpan.FromDays(maxAritcleAge))
+            if (LimitAge && DateTime.Now - File.GetCreationTime(files[i]) > TimeSpan.FromDays(MaxArticleAge))
             {
                 File.Delete(files[i]);
                 deleted++;
             }
-            if (i - deleted >= maxArticleNumber)
+            if (LimitNumber && i - deleted >= MaxArticleNumber)
             {
                 File.Delete(files[i]);
                 deleted++;
@@ -153,6 +307,7 @@ public class FeedChannelMenuAlt : MenuBlock
 
     public FeedChannelMenuAlt(FeedChannelMenu original, AnchorType anchorType = AnchorType.Cursor) : base(anchorType)
     {
+
         options.Add(new MenuOption("Mark All as Read", this, () => Task.Run(() => original.MarkAllAsRead())));
 
         var priorityOption = new MenuOption($"Switch Priority (current: {(original.lowPriority ? "Low" : "Normal")})", this, () => Task.Run(() => { }));
@@ -160,7 +315,25 @@ public class FeedChannelMenuAlt : MenuBlock
         priorityOption.tip = "Normal priority displays which articles are unread, low priority doesn't. Meant to reduce clutter from feeds that post frequently.";
         options.Add(priorityOption);
 
+        var setArchiveOption = new MenuOption($"Switch Archive Mode (current: {original.PrettyArchiveMode}):", this, () => Task.Run(() => { }));
+        var archiveModeMenu = SetArchiveModeMenu(original, setArchiveOption);
+        setArchiveOption.ChangeOnSelected(() => Task.Run(() => Globals.activeScene.PushMenu(archiveModeMenu)));
+        options.Add(setArchiveOption);
+
         options[cursor].selected = true;
+    }
+
+    private MenuBlock SetArchiveModeMenu(FeedChannelMenu original, MenuOption parentOption)
+    {
+        var menu = new MenuBlock();
+        parentOption.childMenu = menu;
+        foreach (ArchiveMode mode in Enum.GetValues(typeof(ArchiveMode)))
+        {
+            var option = new MenuOption(original.PrettifyArchiveMode(mode, false), menu, () => Task.Run(() => original.SetArchiveMode(mode, menu, parentOption)));
+            menu.options.Add(option);
+        }
+        menu.options[menu.cursor].selected = true;
+        return menu;
     }
 
     protected override void OnUpdate()
--- Scenes/FeedScene.cs
+++ Scenes/FeedScene.cs
@@ -2,6 +2,7 @@ using System.Xml;
 using Newtonsoft.Json;
 using YTCons.MenuBlocks;
 using System.Collections.Concurrent;
+using System.Text;
 
 namespace YTCons.Scenes;
 
@@ -11,44 +12,56 @@ public class FeedScene : Scene
 
     private List<MenuBlock> savedMenus = new();
 
+    public ArchiveMode defaultArchive = ArchiveMode.ArchiveAll;
+    public int maxArticleAge = 60;
+    public int maxArticleNumber = 1000;
+
+    public FolderMenu root = new FolderMenu("root", null, AnchorType.Center);
+
     public static async Task<FeedScene> CreateAsync()
     {
         var instance = new FeedScene();
-        string path = Globals.ReadLine(0, 0, "Enter the path of the opml file you want to import: ");
-        while (!File.Exists(path) && !path.EndsWith(".opml"))
+        string path = Path.Combine(Dirs.feedsDir, "feeds.opml");
+        if (!File.Exists(path))
         {
             path = Globals.ReadLine(0, 0, "Enter the path of the opml file you want to import: ");
+            while (!File.Exists(path) && !path.EndsWith(".opml"))
+            {
+                path = Globals.ReadLine(0, 0, "Enter the path of the opml file you want to import: ");
+            }
         }
-        using var stream = File.OpenRead(path);
-        using var xmlStream = XmlReader.Create(stream);
-        FolderMenu root = new FolderMenu("root", null, AnchorType.Center);
-        ConcurrentBag<FeedChannelMenu> rootMenus = new();
-        List<Task> menuTasks = new();
-        FolderMenu curFolder = root;
-        while (xmlStream.Read())
+        using (var stream = File.OpenRead(path))
         {
-            if (xmlStream.NodeType == XmlNodeType.Element && xmlStream.GetAttribute("text") != null && xmlStream.GetAttribute("xmlUrl") == null)
+            using var xmlStream = XmlReader.Create(stream);
+            ConcurrentBag<FeedChannelMenu> rootMenus = new();
+            List<Task> menuTasks = new();
+            FolderMenu curFolder = instance.root;
+            while (xmlStream.Read())
             {
-                var folder = new FolderMenu(xmlStream.GetAttribute("text")!, curFolder);
-                curFolder.subFolders.Add(folder);
-                curFolder = folder;
-                continue;
-            }
-            if (xmlStream.NodeType == XmlNodeType.EndElement && xmlStream.Name == "outline")
-            {
-                if (curFolder.parent != null)
+                if (xmlStream.NodeType == XmlNodeType.Element && xmlStream.GetAttribute("text") != null && xmlStream.GetAttribute("xmlUrl") == null)
                 {
-                    curFolder = curFolder.parent;
+                    var folder = new FolderMenu(xmlStream.GetAttribute("text")!, curFolder);
+                    curFolder.subFolders.Add(folder);
+                    curFolder = folder;
+                    continue;
                 }
+                if (xmlStream.NodeType == XmlNodeType.EndElement && xmlStream.Name == "outline")
+                {
+                    if (curFolder.parent != null)
+                    {
+                        curFolder = curFolder.parent;
+                    }
+                }
+                curFolder.CheckForChannels(xmlStream, menuTasks);
             }
-            curFolder.CheckForChannels(xmlStream, menuTasks);
+            await Task.WhenAll(menuTasks);
+            instance.root.RecursiveFolderAdding();
+            instance.root.options.Insert(0, new MenuOption("Back", instance.root, () => Task.Run(() =>
+                            { LoadBar.visible = false; Globals.scenes.Pop(); })));
+            instance.root.options[instance.root.cursor].selected = true;
+            instance.PushMenu(instance.root);
         }
-        await Task.WhenAll(menuTasks);
-        root.RecursiveFolderAdding();
-        root.options.Insert(0, new MenuOption("Back", root, () => Task.Run(() =>
-                        { LoadBar.visible = false; Globals.scenes.Pop(); })));
-        root.options[root.cursor].selected = true;
-        instance.PushMenu(root);
+        instance.SaveAsXml();
         return instance;
     }
 
@@ -56,6 +69,11 @@ public class FeedScene : Scene
     {
         var feedMenu = await FeedChannelMenu.CreateAsync(packet);
         bag.Add(feedMenu);
+    }
+
+    public void SaveAsXml()
+    {
+        root.SaveAsXml();
     }
 }
 
@@ -98,7 +116,9 @@ public class FolderMenu : MenuBlock
             int selectedOffset = selected && parent.confirmed && parent.active ? 2 : 0;
 
             int digits = MenuOption.DivideWithPowers(counter, 10) + 1;
-            for (int l = i + 8 + selectedOffset + digits; l < i + 14 + digits + selectedOffset; l++)
+
+            int useCounterOffset = counter == 0 ? 0 : 4;
+            for (int l = i + 4 + useCounterOffset + selectedOffset + digits; l < i + 10 + useCounterOffset + digits + selectedOffset; l++)
             {
                 Globals.SetForegroundColor(l, j, ConsoleColor.Blue);
             }
@@ -119,20 +139,38 @@ public class FolderMenu : MenuBlock
         if (!xmlStream.GetAttribute("xmlUrl").Contains("https://www.youtube.com/feeds")) return;
         var packet = new FeedChannelMenu.PreMenuPacket()
         {
-            url = xmlStream.GetAttribute("xmlUrl"),
-            title = xmlStream.GetAttribute("title"),
-            notify = xmlStream.GetAttribute("useNotification") == "true"
+            url = xmlStream.GetAttribute("xmlUrl")!,
+            title = xmlStream.GetAttribute("text"),
+            notify = xmlStream.GetAttribute("useNotification") == "true",
+            lowPriority = xmlStream.GetAttribute("markImmediatelyAsRead") == "true"
         };
-        if (xmlStream.GetAttribute("maxAritcleAge") != null)
+        if (xmlStream.GetAttribute("maxArticleAge") != null)
         {
-            packet.maxAritcleAge = Convert.ToInt32(xmlStream.GetAttribute("maxAritcleAge"));
+            packet.maxArticleAge = Convert.ToInt32(xmlStream.GetAttribute("maxArticleAge"));
         }
         if (xmlStream.GetAttribute("maxArticleNumber") != null)
         {
             packet.maxArticleNumber = Convert.ToInt32(xmlStream.GetAttribute("maxArticleNumber"));
         }
-        string url = xmlStream.GetAttribute("xmlUrl");
-        string text = xmlStream.GetAttribute("text");
+        if (xmlStream.GetAttribute("archiveMode") != null)
+        {
+            string archiveMode = xmlStream.GetAttribute("archiveMode")!;
+            switch (archiveMode)
+            {
+                case "limitArticleNumber":
+                    packet.archiveMode = ArchiveMode.LimitNumber;
+                    break;
+                case "limitArticleAge":
+                    packet.archiveMode = ArchiveMode.LimitAge;
+                    break;
+                case "keepAllArticles":
+                    packet.archiveMode = ArchiveMode.ArchiveAll;
+                    break;
+                case "disableArchiving":
+                    packet.archiveMode = ArchiveMode.DontArchive;
+                    break;
+            }
+        }
         menuTasks.Add(FeedScene.MakeFeedMenuAsync(menus, packet));
     }
 
@@ -163,5 +201,82 @@ public class FolderMenu : MenuBlock
             option.useCounter = true;
             options.Add(option);
         }
+    }
+
+    public void SaveAsXml()
+    {
+        var settings = new XmlWriterSettings
+        {
+            Indent = true
+        };
+        settings.ConformanceLevel = ConformanceLevel.Document;
+        using var xmlFile = File.Create(Path.Combine(Dirs.feedsDir, "feeds.opml"));
+        using var writer = XmlWriter.Create(xmlFile, settings);
+        WriteHeader(writer);
+        WriteBody(writer, false);
+        WriteFooter(writer);
+    }
+
+    private void WriteBody(XmlWriter writer, bool writeOutlines = true)
+    {
+        if (writeOutlines) writer.WriteStartElement("outline");
+        writer.WriteAttributeString("text", title);
+
+        foreach (FolderMenu subFolder in subFolders)
+        {
+            subFolder.WriteBody(writer);
+        }
+        foreach (FeedChannelMenu menu in menus)
+        {
+            writer.WriteStartElement("outline");
+            writer.WriteAttributeString("text", menu.title);
+            //writer.WriteAttributeString("title",menu.title);
+            writer.WriteAttributeString("xmlUrl", menu.url);
+            writer.WriteAttributeString("htmlUrl", $"https://www.youtube.com/channel/{menu.id}");
+            writer.WriteAttributeString("archiveMode", XmlArchiveMode(menu.archiveMode));
+            writer.WriteAttributeString("maxArticleAge", menu.MaxArticleAgeRaw.ToString());
+            writer.WriteAttributeString("markImmediatelyAsRead", menu.lowPriority ? "true" : "false");
+            writer.WriteAttributeString("maxArticleNumber", menu.MaxArticleNumberRaw.ToString());
+            writer.WriteAttributeString("type", "rss");
+            writer.WriteAttributeString("version", "RSS");
+            writer.WriteEndElement();
+        }
+
+        if (writeOutlines) writer.WriteEndElement();
+    }
+
+    private string XmlArchiveMode(ArchiveMode archiveMode)
+    {
+        switch (archiveMode)
+        {
+            case ArchiveMode.LimitAge:
+                return "limitArticleAge";
+            case ArchiveMode.DontArchive:
+                return "disableArchiving";
+            case ArchiveMode.ArchiveAll:
+                return "keepAllArticles";
+            case ArchiveMode.LimitNumber:
+                return "limitArticleNumber";
+            case ArchiveMode.Default:
+            default:
+                return "globalDefault";
+        }
+    }
+
+    private void WriteHeader(XmlWriter writer)
+    {
+        writer.WriteStartElement("opml");
+        writer.WriteAttributeString("version", "1.0");
+        writer.WriteStartElement("head");
+        writer.WriteStartElement("text");
+        writer.WriteEndElement();
+        writer.WriteEndElement();
+        writer.WriteStartElement("body");
+    }
+
+    private void WriteFooter(XmlWriter writer)
+    {
+        writer.WriteEndElement();
+        writer.WriteEndElement();
     }
 }
